364f233ead12b62a18ef53d68d1cecf201b45c7d
Jiří Vrba <jiri@vrba.dev>
2025-03-05 22:40:10 +0100

    Basic layout

--- lib/jamify_web.ex
+++ lib/jamify_web.ex
@@ -17,7 +17,7 @@ defmodule JamifyWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images icon.gif robots.txt)
 
   def router do
     quote do
